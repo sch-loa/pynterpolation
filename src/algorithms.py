fb5a783b8c65b54804250004d60960f5718d83ed
loana <laschleichgarcia@estudiantes.unsam.edu.ar>
2023-06-04 22:34:44 -0300

    interpolacion de newton terminada + explicaciones del metodo

--- src/algorithms.py
+++ src/algorithms.py
@@ -3,32 +3,32 @@ import numpy as np
 from sympy import symbols, Eq, Poly, solve
 
 
-def polinomio_interpolador_newton():
+def polinomio_interpolador_newton(pares_xy):
     x = symbols('x')
     c = symbols('c')
 
-    pares_xy = generador_pares_random(20)
-    #pares_xy = [(1,2),(3,4),(4,-1)]
     p_factores = factor_polinomio(pares_xy)
 
-
-    x0, y0 = pares_xy[0]
+    _, y0 = pares_xy[0]
     p0 = y0
     for x1, y1 in pares_xy[1:]:
         p1 = p0 + c * next(p_factores)
         c_1 = calcular_coeficiente(p1, (x1, y1), (x, c))
         p1 = p1.replace(c, c_1)
-        #print(p1)
-        #print(c_1)
 
         p0 = p1
-    return (pares_xy, p0, x)
+
+    return (p0, x)
+
+
 def polinomio_interpolador_lagrange():
     pass
 
 def polinomio_interpolador_diferencias_divididas():
     pass
 
+# Calcula el coeficiente del polinomio reemplazando el valor
+# de la variable simbólica x y despejando c
 def calcular_coeficiente(p1, valores, simbolos):
     x1, y1 = valores
     x, c = simbolos
@@ -53,3 +53,7 @@ def generador_pares_random(cantidad):
 
 def grado_polinomio(polinomio):
     return Poly(polinomio).degree()
+
+# Compara los coeficientes de una lista de polinomios entre sí
+def coeficientes_iguales(p0, p1, x):
+    return Poly(p0, x).all_coeffs() == Poly(p1, x).all_coeffs()
--- src/main.py
+++ src/main.py
@@ -3,28 +3,98 @@ import random as rd
 import numpy as np
 import sympy as sp
 
-from algorithms import polinomio_interpolador_newton
+from algorithms import polinomio_interpolador_newton, generador_pares_random, grado_polinomio, coeficientes_iguales
 
-pares_xy, polinomio, x_simb = polinomio_interpolador_newton()
+METODO_NEWTON_CARTEL = """
+ ____________________________________________________________________________
+|                                                                            |
+|                      POLINOMIO INTERPOLADOR DE NEWTON                      |
+|____________________________________________________________________________|
+|                                                                            |
+|  INTEGRANTES:                                                              |
+|  |_ Loana Abril Schleich Garcia.                                           |
+|____________________________________________________________________________|
+|                                                                            |
+|                        FUNCIONAMIENTO DEL ALGORITMO                        |
+|____________________________________________________________________________|
+|                                                                            |
+|  El método funciona iterativamente. Dada una lista de pares ordenados se   |
+|  la recorre en busca de un polinomio que pase por ese punto y todos los    |
+|  anteriores en la colección. En cada iteración se reutiliza el polinomio   |
+|  encontrado de forma tal que se le agrege un nuevo punto.                  |
+|  Matemáticamente, esto es:                                                 |
+|                                                                            |
+|   P_n(x) = c0 + c1(x-x_0)+ c2(x-x_0)(x-x_1) + ... + cn(x-x0)...(x-x_n-1)   |
+|                                                                            |
+|  Siendo n el número de pares ordenados y c el coeficiente de cada factor.  |
+|  Cada coeficiente se halla en la corriente iteración agregando los nuevos  |
+|  términos y reemplazando los valores (x, y) del actual par ordenado.       |
+|                                                                            |
+|  Se sabe además que el grado del polinomio es menor o igual al número de   |
+|  pares ordenados.                                                          |
+|                                                                            |
+|  El algoritmo no depende del orden en el que se encuentren los datos,      |
+|  esto se verifica interpolando el mismo conjunto de números, invirtiendo   |
+|  el orden y cambiándolo aleatoriamente.                                    |
+|____________________________________________________________________________|
+                    """
 
+IGUALDAD_POLINOMIOS_CARTEL = """\033[F
+ ____________________________________________________________________________
+|                                                                            |
+|                          IGUALDAD ENTRE POLINOMIOS                         |
+|____________________________________________________________________________|
+|                                                                            |
+|  Para comparar la igualdad entre dos polinomios, es posible comparar sus   |
+|  coeficientes. Si los coeficientes de los términos correspondientes        |
+|  (de esto se presume también que tienen el mismo grado) son idénticos en   |
+|  ambos, entonces los mismos son iguales.                                   |
+|____________________________________________________________________________|   
+                            """
+
+# Polinomio interpolador para el conjunto de datos original
+pares_xy = generador_pares_random(20)
+polinomio, x_simb = polinomio_interpolador_newton(pares_xy)
+
+# Polinomio interpolador para el conjunto de datos invertido
 pares_xy_invertidos = pares_xy[::-1]
+polinomio_invertido, x_simb_invertido = polinomio_interpolador_newton(pares_xy)
+
+# Polinomio interpolador para el conjunto de datos desordenado aleatoriamente
 pares_xy_desordenados = rd.sample(pares_xy, len(pares_xy))
+polinomio_desordenado, x_simb_desordenado = polinomio_interpolador_newton(pares_xy)
+
+
+PARES_XY = [pares_xy, pares_xy_invertidos, pares_xy_desordenados]
+POLINOMIOS = [polinomio, polinomio_invertido, polinomio_desordenado]
+X_SIMBS = [x_simb, x_simb_invertido, x_simb_desordenado]
+
+print(METODO_NEWTON_CARTEL)
+print(f'   El grado del polinomio es: {grado_polinomio(polinomio)}')
+print(IGUALDAD_POLINOMIOS_CARTEL)
+if(coeficientes_iguales(polinomio, polinomio_invertido, x_simb) and coeficientes_iguales(polinomio, polinomio_desordenado, x_simb)):
+    print('   Los coeficientes de los tres polinomios son exactamente iguales.\n')
 
 # GRAFICOS DE LOS POLINOMIOS Y PARES ORDENADOS
-func_polinomica = sp.lambdify(x_simb, polinomio, 'numpy')
-fig, ax = plt.subplots()
+TITULOS = ['Ordenados', 'Invertidos', 'Desordenados']
+COLORES = ['r', 'c', 'y']
 
 x = np.linspace(-30, 30, 300)
-ax.plot(x, func_polinomica(x), 'k')
 
-for x_n, y_n in pares_xy:
-    ax.scatter([x_n],[y_n], color='r')
+# Creo un gráfico por cada polinomio
+for i in range(len(PARES_XY)):
+    func_polinomica = sp.lambdify(X_SIMBS[i], POLINOMIOS[i], 'numpy')
+    fig, ax = plt.subplots()
 
+    ax.plot(x, func_polinomica(x), 'k')
 
-plt.xlim(-18, 18)
-plt.ylim(-18, 18)
+    for x_n, y_n in PARES_XY[i]:
+        ax.scatter([x_n],[y_n], color = COLORES[i])
 
-plt.grid(True, linestyle='--', linewidth=0.5, color='gray') 
-plt.title('Polinomio Interpolador de Newton vs. Pares Ordenados')
+    plt.xlim(-18, 18)
+    plt.ylim(-18, 18)
+
+    plt.grid(True, linestyle='--', linewidth=0.5, color='gray') 
+    plt.title(f'Polinomio Interpolador de Newton vs. Pares {TITULOS[i]}')
 
 plt.show()
